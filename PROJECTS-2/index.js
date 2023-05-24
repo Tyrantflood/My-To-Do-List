31f5b71ceb2c9f5e5266b05ef7bd1e575b09914c
Tyrantflood <99040712+Tyrantflood@users.noreply.github.com>
2023-05-24 12:06:16 +0100

    Update index.js

--- PROJECTS-2/index.js
+++ PROJECTS-2/index.js
@@ -7,19 +7,43 @@ window.addEventListener("load", () => {
     s.preventDefault();
 
     const task = input.value;
+
     // manipulate the DOM to add a new class element inside
+    // with the inputed value
+
     const task_el = document.createElement("div");
     task_el.classList.add("task-list");
 
     const task_content_el = document.createElement("div");
-
     task_content_el.classList.add("task-content");
 
     task_content_el.innerText = task;
 
     task_el.appendChild(task_content_el);
-
     list_el.appendChild(task_el);
-    
+
+    const task_input_el = document.createElement("input");
+    task_input_el.classList.add("text");
+    task_input_el.type = "text";
+    task_input_el.value = task;
+    task_input_el.setAttribute(("readonly", "readonly"));
+
+    task_content_el.appendChild(task_input_el);
+
+    const task_action_el = document.createElement("div");
+    task_action_el.classList.add("task-actions");
+
+    const task_edit = document.createElement("button");
+    task_edit.classList.add("edit");
+    task_edit.innerHTML = "Edit";
+
+    const task_delete = document.createElement("button");
+    task_delete.classList.add("delete");
+    task_delete.innerHTML = "Delete";
+
+    task_action_el.appendChild(task_edit);
+    task_action_el.appendChild(task_delete);
+
+    task_el.appendChild(task_action_el);
   });
 });
